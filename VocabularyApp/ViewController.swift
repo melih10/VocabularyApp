b8ab2aa060fb47a274de268a00a54139839bf7b2
melih10 <94173500+melih10@users.noreply.github.com>
2022-12-06 22:09:10 +0300

    Storyboard design...

--- VocabularyApp/ViewController.swift
+++ VocabularyApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText1: UILabel!
+    @IBOutlet weak var labelText2: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
